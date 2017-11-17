cf2c7186894039e4609c228caf9d600fea51239c
Marcin Smoczyński <msmoczynski@enigma.com.pl>
2017-11-17 16:30:27 +0100

    don't build mpu wrappers, don't use nano clib

--- mk/kx.mk
+++ mk/kx.mk
@@ -2,4 +2,4 @@ ARCH= cortexm4f
 TOOLCHAIN= arm-none-eabi
 CCFLAGS+= -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16
 CFLAGS+= -ffunction-sections -fdata-sections
-LDFLAGS+= -Xlinker --gc-sections -specs=nano.specs -specs=nosys.specs
+LDFLAGS+= -Xlinker --gc-sections -specs=nosys.specs
